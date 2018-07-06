1a22e45303ee5c7341b4e224077fcf2cd23deedd
Tom Rein <tomjrein@gmail.com>
2018-07-06 04:29:05 -0400

    added production logic for heroku

--- backend/server.js
+++ backend/server.js
@@ -61,6 +61,15 @@ router.put('/players/:playerId', (req, res) => {
   });
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 app.use('/api', router);
 //app.use(express.static(path.join(__dirname, 'client/build')));
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
