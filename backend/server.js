53ea593d21d9264b51d498ed8d267d8f448c14e0
Tom Rein <tomjrein@gmail.com>
2018-07-06 03:33:46 -0400

    added dependency

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 const app = express();
+const path = require('path');
 const Player = require('./models/players.js');
 const cors = require('cors');
 const bodyParser = require('body-parser');
